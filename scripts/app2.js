218334b1bc8a073211048849ecbbca38bbacb843
Buba228 <55086111+drocsfox11@users.noreply.github.com>
2023-10-31 16:08:05 +0300

    Add files via upload

--- scripts/app2.js
+++ scripts/app2.js
@@ -143,52 +143,131 @@ const results =
 //Массив с вопросами
 const questions = 
 [
-	new Question("Как расшифровывается CSS?", 
+	new Question("Что делает свойство 'display: none;'?", 
 	[
-		new Answer("Computer Style Sheets", 0),
-		new Answer("Cascading Style Sheets", 1),
-		new Answer("Common Style Sheets", 0),
+		new Answer("Скрывает элемент, но он продолжает занимать пространство на странице", 0),
+		new Answer("Удаляет элемент со страницы", 0),
+		new Answer("Скрывает элемент, и он не занимает пространство на странице", 1),
+		new Answer("Делает элемент прозрачным", 0)
 	]),
-
-	new Question("Выберите синтаксически корректное CSS правило", 
+	new Question("Какое свойство используется для изменения цвета текста?", 
 	[
-		new Answer("div:[font_size-2em]", 0),
-		new Answer("div [font-size:2em]", 0),
-		new Answer("div {font-size:2em}", 1),
-		new Answer("div {font_size:2em}", 0)
+		new Answer("text-color", 0),
+		new Answer("color", 1),
+		new Answer("font-color", 0),
+		new Answer("text-style", 0)
 	]),
-
-	new Question("Укажите CSS свойство позволяющее устанавливать размер шрифта?", 
+	new Question("Каким будет результат работы следующего CSS-кода: body { font-size: 100%; }?", 
+	[
+		new Answer("Размер шрифта текста в теге body будет увеличен в 2 раза", 0),
+		new Answer("Размер шрифта текста в теге body будет равен размеру шрифта по умолчанию", 1),
+		new Answer("Размер шрифта текста в теге body уменьшится до 0", 0),
+		new Answer("Код не будет иметь никакого эффекта", 0)
+	]),
+	new Question("Какой селектор выбирает все элементы &ltp&gt, находящиеся внутри элементов &ltdiv&gt?", 
+	[
+		new Answer("div + p", 0),
+		new Answer("div p", 1),
+		new Answer("div > p", 0),
+		new Answer("p > div", 0)
+	]),
+	new Question("Какое значение свойства position делает элемент позиционированным относительно его исходного местоположения?", 
+	[
+		new Answer("absolute", 0),
+		new Answer("fixed", 0),
+		new Answer("relative", 1),
+		new Answer("static", 0)
+	]),
+	new Question("Какое свойство используется для изменения прозрачности элемента?", 
+	[
+		new Answer("opacity", 1),
+		new Answer("transparent", 0),
+		new Answer("visibility", 0),
+		new Answer("alpha", 0)
+	]),
+	new Question("Что такое псевдокласс в CSS?", 
+	[
+		new Answer("Специальный класс, который добавляется в HTML", 0),
+		new Answer("Селектор, который определяет особое состояние элемента", 1),
+		new Answer("Тег, который используется только в CSS.", 0),
+		new Answer("Класс, который автоматически применяется ко всем элементам", 0)
+	]),
+	new Question("Какой селектор CSS используется для стилизации элементов с определенным id?", 
+	[
+		new Answer("`.` (точка)", 0),
+		new Answer("`#` (решетка)", 1),
+		new Answer("`:` (двоеточие)", 0),
+		new Answer("`@` (собачка)", 0)
+	]),
+	new Question("Какое свойство позволяет контролировать размер шрифта?", 
 	[
 		new Answer("font-weight", 0),
+		new Answer("font-style", 0),
 		new Answer("font-size", 1),
-		new Answer("size", 0),
-		new Answer("weight", 0)
+		new Answer("font-family", 0)
 	]),
-
-	new Question('Укажите селектор позволяющий выбрать все элементы div имеющие атрибут id="wrap"', 
+	new Question("Что делает padding: 20px 10px 5px;?", 
 	[
-		new Answer("div-wrap", 1),
-		new Answer("div id.wrap", 0),
-		new Answer("div.wrap", 0),
-		new Answer("div#wrap", 0)
+		new Answer("Устанавливает внешний отступ 20px сверху, 10px справа и слева, 5px снизу", 0),
+		new Answer("Устанавливает внутренний отступ 20px сверху, 10px справа и слева, 5px снизу", 1),
+		new Answer("Устанавливает внешний отступ 20px сверху, 10px справа, 5px снизу и слева", 0),
+		new Answer("Устанавливает внутренний отступ 20px сверху, 10px справа, 5px снизу и слева", 0)
 	]),
-
-
-	new Question("2 + 2 * 2 = ", 
+	new Question("Что такое `em` в CSS?", 
 	[
-		new Answer("4", 0),
-		new Answer("6", 1),
-		new Answer("8", 0),
-		new Answer("10", 0)
+		new Answer("Абсолютная единица измерения, равная ширине одного пикселя", 0),
+		new Answer("Относительная единица измерения, основанная на размере шрифта родительского элемента", 1),
+		new Answer("Единица измерения для цветов", 0),
+		new Answer("Глобальная константа для ширины элементов", 0)
 	]),
-
-	new Question("2 + 2 / 2 = ", 
+	new Question("Какое значение свойства position позволяет элементу оставаться на фиксированном месте при прокрутке страницы?", 
 	[
-		new Answer("1", 0),
-		new Answer("2", 0),
-		new Answer("3", 1),
-		new Answer("4", 0)
+		new Answer("relative", 0),
+		new Answer("absolute", 0),
+		new Answer("fixed", 1),
+		new Answer("static", 0)
+	]),
+	new Question("Какое свойство CSS задаёт тень элемента?", 
+	[
+		new Answer("box-shadow", 1),
+		new Answer("text-shadow", 0),
+		new Answer("border-shadow", 0),
+		new Answer("element-shadow", 0)
+	]),
+	new Question("Какой селектор выбирает элемент (F), непосредственно следующий за определённым элементом (E), если у них общий родитель?", 
+	[
+		new Answer("E ~ F", 0),
+		new Answer("E + F", 1),
+		new Answer("E > F", 0),
+		new Answer("E F", 0)
+	]),
+	new Question("Что делает font-weight: bold;?", 
+	[
+		new Answer("Изменяет размер шрифта на более крупный", 0),
+		new Answer("Делает текст жирным", 0),
+		new Answer("Меняет шрифт на полужирный", 1),
+		new Answer("Устанавливает специальный шрифт для заголовков", 0)
+	]),
+	new Question("Какое свойство задаёт внешний отступ элемента?", 
+	[
+		new Answer("padding", 0),
+		new Answer("margin", 1),
+		new Answer("border", 0),
+		new Answer("outline", 0)
+	]),
+	new Question("Что делает свойство 'text-transform: uppercase;'?", 
+	[
+		new Answer("Переводит все символы текста в верхний регистр", 1),
+		new Answer("Увеличивает размер текста", 0),
+		new Answer("Делает текст жирным", 0),
+		new Answer("Изменяет шрифт на капитель", 0)
+	]),
+	new Question("Что означает 'color: rgba(0,0,0,0.5);?'", 
+	[
+		new Answer("Черный цвет с 50% прозрачностью", 1),
+		new Answer("Белый цвет с 50% прозрачностью", 0),
+		new Answer("Красный цвет с 50% прозрачностью", 0),
+		new Answer("Прозрачный цвет", 0)
 	])
 ];
 
